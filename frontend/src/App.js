15272fe20cc8e1514f33c9aa8aa8d50cbfe741fe
olivermusgrove <99029469+olivermusgrove@users.noreply.github.com>
2022-04-29 15:55:24 +0000

    materialui table added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import ChemicalTable from "./components/ChemicalTable";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          Hello World
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <ChemicalTable />
   );
 }
 
